ef5728f455f47e337326f054ea90239b0f9680db
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-06-01 12:19:39 +0530

    strings completed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("something")
+// string
+//way to write string 
+
+console.log("hello");
+console.log("hello");
+console.log(`hello`);
+//accessing index of the string 
+console.log("Hello World"[10]);
+//accesing .length property with string 
+console.log("Hello World".length);
+let str="saravana";
+console.log(str.length);
+console.log(str[str.length-1]);
+
